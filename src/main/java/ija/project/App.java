4b353fd388437c625d51d6eeec6de1163fb9bba0
Rudolf Hyksa <xhyksa00@stud.fit.vutbr.cz>
2022-04-08 23:17:00 +0200

    added bare project structure

---
+++ src/main/java/ija/project/App.java
@@ -0,0 +1,13 @@
+package ija.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
