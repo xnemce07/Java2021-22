d434b4a61688199110b980d118088d47aca40091
Leopold Nemček <xnemce07@stud.fit.vutbr.cz>
2022-04-09 18:23:15 +0200

    Added some comments

--- src/main/java/ija/project/model/classdiagram/UMLClass.java
+++ src/main/java/ija/project/model/classdiagram/UMLClass.java
@@ -14,22 +14,44 @@ public class UMLClass extends UMLClassifier{
     }
 
     //
+
+    /**
+     * Create and add attribute with type, that is not user defined
+     * @param name Name of the attribute
+     * @param type Type of the attribute
+     * @return UUID of created attribute
+     */
     public UUID addAttribute(String name,String type){
         UMLAttribute attr = new UMLAttribute(name,type);
         attributes.add(attr);
         return attr.getId();
     }
 
+    /**
+     * Create and add attribute with type that already exists
+     * @param name Name of the attribute
+     * @param type Type of the attribute
+     * @return UUID of created attribute
+     */
     public UUID addAttribute(String name,UMLClassifier type){
         UMLAttribute attr = new UMLAttribute(name,type);
         attributes.add(attr);
         return attr.getId();
     }
 
+    /**
+     * Add already created attribute
+     * @param attribute Attribute to be added
+     */
     public void addAttribute(UMLAttribute attribute){
         attributes.add(attribute);
     }
 
+    /**
+     * Get attribute by UUID
+     * @param id UUID
+     * @return Attribute with specified UUID, null if none was found
+     */
     public UMLAttribute getAttribute(UUID id){
         for (UMLAttribute attribute:attributes){
             if(attribute.getId() == id){
@@ -39,40 +61,81 @@ public class UMLClass extends UMLClassifier{
         return null;
     }
 
+    /**
+     * Get attribute by index
+     * @param index Index
+     * @throws IndexOutOfBoundsException – if the index is out of range (index < 0 || index >= size(attributes))
+     * @return Attribute
+     */
     public  UMLAttribute getAttribute(int index){
         return attributes.get(index);
     }
 
+    /**
+     * Remove attribute by index
+     * @param index
+     * @throws IndexOutOfBoundsException – if the index is out of range (index < 0 || index >= size(attributes))
+     */
     public void removeAttribute(int index){
         attributes.remove(index);
     }
 
+    /**
+     * Remove attribute by UUID
+     * @param id UUID
+     */
     public void removeAttribute(UUID id){
         attributes.remove(getAttribute(id));
     }
 
+    /**
+     * Remove attribute
+     * @param attribute
+     */
     public void removeAttribute(UMLAttribute attribute){
         attributes.remove(attribute);
     }
 
 
     //
+
+    /**
+     * Create and add method with type, that is not user defined
+     * @param name Name of the method
+     * @param type Type of the method
+     * @return UUID of created method
+     */
     public UUID addMethod(String name,String type){
         UMLMethod method = new UMLMethod(name,type);
         methods.add(method);
         return method.getId();
     }
 
+    /**
+     * Create and add method with type that already exists
+     * @param name Name of the method
+     * @param type Type of the method
+     * @return UUID of created method
+     */
     public UUID addMethod(String name,UMLClassifier type){
         UMLMethod method = new UMLMethod(name,type);
         methods.add(method);
         return method.getId();
     }
 
+    /**
+     * Add already created method
+     * @param method Method to be added
+     */
     public void addMethod(UMLMethod method){
         methods.add(method);
     }
 
+    /**
+     * Get method with specified UUID
+     * @param id UUID
+     * @return Method with UUID, null if none was found
+     */
     public UMLMethod getMethod(UUID id){
         for (UMLMethod method:methods){
             if (method.getId() == id){
@@ -82,18 +145,37 @@ public class UMLClass extends UMLClassifier{
         return null;
     }
 
+    /**
+     * Get method by index
+     * @param index
+     * @throws IndexOutOfBoundsException – if the index is out of range (index < 0 || index >= size(methods))
+     */
     public  UMLMethod getMethod(int index){
         return methods.get(index);
     }
 
+    /**
+     * Remove method by index
+     * @param index
+     * @throws IndexOutOfBoundsException – if the index is out of range (index < 0 || index >= size(methods))
+     */
     public void removeMethod(int index){
         methods.remove(index);
     }
 
+
+    /**
+     * Remove method by UUID
+     * @param id UUID
+     */
     public void removeMethod(UUID id){
         methods.remove(getMethod(id));
     }
 
+    /**
+     * Remove method
+     * @param Method
+     */
     public void removeMethod(UMLMethod Method){
         methods.remove(Method);
     }
--- src/main/java/ija/project/model/classdiagram/UMLMethod.java
+++ src/main/java/ija/project/model/classdiagram/UMLMethod.java
@@ -1,7 +1,9 @@
+
 package ija.project.model.classdiagram;
 
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class UMLMethod extends UMLAttribute{
 
@@ -15,22 +17,44 @@ public class UMLMethod extends UMLAttribute{
         super(name,type);
     }
 
+    /**
+     * Create and add UMLClassifier to parameter list
+     * @param name Name of the new UMLClassifier
+     */
     public void addParam(String name){
         parameters.add(UMLClassifier.forName(name));
     }
 
+
+    /**
+     * Add UMLClassifier to list of parameter list
+     * @param param Created UMLClassifier
+     */
     public void addParam(UMLClassifier param){
         parameters.add(param);
     }
 
+    /**
+     * Remove parameter by index
+     * @param index Index of removed parameter
+     */
     public void removeParam(int index){
         parameters.remove(index);
     }
 
+    /**
+     * Remove parameter
+     * @param param Parameter to be removed
+     */
     public void removeParam(UMLClassifier param){
         parameters.remove(param);
     }
 
+    /**
+     * Get parameter by index
+     * @param index Index
+     * @return Parameter from index
+     */
     public UMLClassifier getParam(int index){
         return parameters.get(index);
     }
